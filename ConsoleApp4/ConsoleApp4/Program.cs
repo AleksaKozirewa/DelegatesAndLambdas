bec57dfb1f5856f165c56d24dd3053168042a03e
Kozyreva Aleksandra <Alexandra.Kozyreva@emergn.com>
2020-05-20 14:29:17 +0300

    Added project ConsoleApp4_Filter_return_bool

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        delegate bool IsEqual(int x);
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            bool targetNumbers = Operation(numbers, x => x < 4);
+
+            Console.WriteLine(targetNumbers);
+            Console.ReadKey();
+        }
+
+        private static bool Operation(int[] numbers, IsEqual func)
+        {
+            foreach (int number in numbers)
+            {
+                if (func(number))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
