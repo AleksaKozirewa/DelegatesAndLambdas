18d2fdda4848b11c4281d5e18cae5adac776d151
Kozyreva Aleksandra <Alexandra.Kozyreva@emergn.com>
2020-05-20 14:25:12 +0300

    Added project ConsoleApp2_MaxValue

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        delegate void CompareOperation(int x, int y);
+
+        static void Main(string[] args)
+        {
+            CompareOperation op = ComparingTwo;
+
+            ComparingTwo(6, 3);
+
+            Console.ReadKey();
+
+        }
+
+        private static void ComparingTwo(int x, int y)
+        {
+            if (x < y)
+            {
+                Console.WriteLine(y + " наибольшее число");
+            }
+            else
+            {
+                Console.WriteLine(x + " наибольшее число");
+            }
+        }
+    }
+}
