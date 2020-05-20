60470186cf92012b2831e27e2e46cede85f89146
Kozyreva Aleksandra <Alexandra.Kozyreva@emergn.com>
2020-05-20 14:19:20 +0300

    Added project ConsoleApp1_Timer

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Example
+    {
+        delegate void Message();
+
+        static void Main(string[] args)
+        {
+            Message mes;
+            int seconds = 3;
+
+            for (int i = 0; i < seconds; i++)
+            {
+                Console.WriteLine("Введите 1, чтобы прошла 1 секунда");
+                int a = Convert.ToInt32(Console.ReadLine());
+                if (a != 1)
+                {
+                    Console.WriteLine("Вы ввели не 1");
+                    i = i - 1;
+                }
+                else if (i == 2)
+                {
+                    mes = TimeIsUp;
+                    mes();
+                }
+            }
+            Console.ReadKey();
+        }
+        private static void TimeIsUp()
+        {
+            Console.WriteLine("Прошло 3 секунды");
+        }
+    }
+}
