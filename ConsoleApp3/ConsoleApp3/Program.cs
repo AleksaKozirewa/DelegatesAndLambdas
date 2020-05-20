ace3aa79eacc7a26ac28f9d7fdf5c9ef315f9844
Kozyreva Aleksandra <Alexandra.Kozyreva@emergn.com>
2020-05-20 14:27:19 +0300

    Added project ConsoleApp3_Filter_return_elements

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        delegate bool IsEqual(int x);
+        static void Main(string[] args)
+        {
+            int[] numbers = {1, 2, 3, 4, 5};
+
+            object targetNumbers = Operation(numbers, x => x < 4);
+            
+            Console.WriteLine(targetNumbers);
+            Console.ReadKey();
+        }
+        private static object Operation(int[] numbers, IsEqual func)
+        {
+            Queue targetNumbers = new Queue();
+            foreach (int number in numbers)
+            {
+                if (func(number))
+                {
+                    targetNumbers.Enqueue(number);
+                    
+                }
+
+            }
+
+            return targetNumbers;
+        }
+
+    }
+}
