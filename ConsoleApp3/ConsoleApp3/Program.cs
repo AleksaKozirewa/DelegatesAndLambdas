aa1adcb09322ee2eddc9cae7ba5bcb94ecc54812
Kozyreva Aleksandra <Alexandra.Kozyreva@emergn.com>
2020-05-21 14:20:35 +0300

    Made changes in ConsoleApp3_Filter_return_elements

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Security.Cryptography.X509Certificates;
 
@@ -8,30 +9,31 @@ namespace ConsoleApp3
     class Program
     {
         delegate bool IsEqual(int x);
+
         static void Main(string[] args)
         {
             int[] numbers = {1, 2, 3, 4, 5};
-
-            object targetNumbers = Operation(numbers, x => x < 4);
             
-            Console.WriteLine(targetNumbers);
+            List<int> targetNumbers = Operation(numbers, x => x < 4);
+
+            string targetNumbersOutput = string.Join(", ", targetNumbers);
+
+            Console.WriteLine(targetNumbersOutput);
             Console.ReadKey();
         }
-        private static object Operation(int[] numbers, IsEqual func)
+
+        private static List<int> Operation(int[] numbers, IsEqual func)
         {
-            Queue targetNumbers = new Queue();
+            List<int> targetNumbers = new List<int>();
+
             foreach (int number in numbers)
             {
                 if (func(number))
                 {
-                    targetNumbers.Enqueue(number);
-                    
+                    targetNumbers.Add(number);
                 }
-
             }
-
             return targetNumbers;
         }
-
     }
 }
